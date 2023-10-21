e6cb411162ad577ff89bc847acc137f871869c3b
Siby C R <sibycr18@gmail.com>
2023-10-21 20:33:12 +0530

    problem solved

--- sibycr18/odd_doubler.py
+++ sibycr18/odd_doubler.py
@@ -1,4 +1,4 @@
-def odd_doubler():
+def odd_doubler(integer_list):
     """
     task : complete the function odd_doubler which doubles the value of all the odd numbers in the list it receives and returns it
     - accepts a list of integers
@@ -9,4 +9,7 @@ def odd_doubler():
     output -> [2, 2, 6, 4, 10]
     """
     
-    pass
+    for index, integer in enumerate(integer_list):
+        if integer % 2 == 1:
+            integer_list[index] = integer * 2
+    return integer_list
