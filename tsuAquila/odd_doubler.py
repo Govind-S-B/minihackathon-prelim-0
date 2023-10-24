6df69d94dc7cad34ce951fa4bc45271c4ab9cf27
tsuAquila <reenacottage@gmail.com>
2023-10-24 19:44:43 +0530

    wadu hek

--- tsuAquila/odd_doubler.py
+++ tsuAquila/odd_doubler.py
@@ -1,4 +1,4 @@
-def odd_doubler():
+def odd_doubler(arr):
     """
     task : complete the function odd_doubler which doubles the value of all the odd numbers in the list it receives and returns it
     - accepts a list of integers
@@ -8,5 +8,14 @@ def odd_doubler():
     input -> [1, 2, 3, 4, 5]
     output -> [2, 2, 6, 4, 10]
     """
+
+    if len(arr) == 0:
+        return []
+
+    for i in range (0, len(arr)):
+        if arr[i] % 2 != 0:
+            arr[i] = arr[i] * 2
+    
+    return arr
     
     pass
